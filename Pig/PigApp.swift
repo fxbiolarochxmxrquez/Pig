ce044ac7c13a5a44fb0187d60aba47c62c7dcbec
Fabiola Rocha Marquez <frochamarquez@bsd220.org>
2023-10-27 09:52:09 -0500

    Initial Commit

---
+++ Pig/PigApp.swift
@@ -0,0 +1,17 @@
+//
+//  PigApp.swift
+//  Pig
+//
+//  Created by Fabiola Rocha Marquez on 10/27/23.
+//
+
+import SwiftUI
+
+@main
+struct PigApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
